434eaeea31fc9d7a937642c35c0a91c0660fd437
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-26 23:06:15 +0530

    fixes

--- automate.py
+++ automate.py
@@ -11,7 +11,8 @@ try:
         if device.action == 'add':
             # some function to run on insertion of usb
             if(device.device_type=='usb_interface'):
-                print usb.get_mount_points()[0][1]
+                if (len(usb.get_mount_points)>0 and len(usb.get_mount_points)[0]>0):
+                    print usb.get_mount_points()[0][1]
 
        # else:
             # some function to run on removal of usb
