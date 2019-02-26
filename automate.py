cc9ac7a43ee93d9dddbf4377c4d02838c172320e
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-27 00:22:43 +0530

    fixes

--- automate.py
+++ automate.py
@@ -34,7 +34,7 @@ def find_that_file(path):
 
 def find_that_file_latest(path):
     files_path = os.path.join(path, '*csv')
-    list_of_files = glob.glob(files_path) # * means all if need specific format then *.csv
+    list_of_files = glob.iglob(files_path) # * means all if need specific format then *.csv
     print list_of_files
     latest_file = max(list_of_files, key=os.path.getctime)
     print latest_file
