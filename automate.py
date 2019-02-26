7a804a9dcf7075820e3bd3e03828d4da43773ae6
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-27 00:26:41 +0530

    fixes

--- automate.py
+++ automate.py
@@ -35,8 +35,7 @@ def find_that_file(path):
 def find_that_file_latest(path):
     files_path = os.path.join(path, '*.csv')
     list_of_files = glob.iglob(files_path) # * means all if need specific format then *.csv
-    print list_of_files
-    latest_file = max(list_of_files, key=os.path.getctime)
+    latest_file = max(list_of_files, key=lambda p: p.stat().st_ctime)
     print latest_file
 
 def get_usb_devices():
