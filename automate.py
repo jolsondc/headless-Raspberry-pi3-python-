07c00dcd7d1a578a74eb6103499091f5a5a7dab6
jolsondc@gmail.com <jolsondc@gmail.com>
2019-02-28 00:00:57 +0530

    commit initial

--- automate.py
+++ automate.py
@@ -37,7 +37,6 @@ def find_that_file(path):
 def copy_file_to_dest(src):
     dst='/home/pi/storage/data.csv'
     os.makedirs(os.path.dirname(dst)
-    os.path.exists(str(src))
     copy2(str(src), dst)#, ignore=ignore_patterns('*.pyc', 'tmp*'))    
     print "copied"
 
